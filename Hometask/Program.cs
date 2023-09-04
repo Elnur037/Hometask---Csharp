2362c51c374c4154732cdcb79dfb21bb5ccb71dd
Elnur037 <elnuriga@code.edu.az>
2023-09-04 20:20:11 +0400

    Big-o calculate

--- Hometask/Program.cs
+++ Hometask/Program.cs
@@ -1,14 +1,14 @@
 ﻿
 
-//TASK-A
+//TASK - A
 
 //void FindHighNumber(int[] array, int limit)
 //{
-//    for (int i = 0; i < array.Length; i++)     
+//    for (int i = 0; i < array.Length; i++) n
 //    {
-//        if (array[i] > limit)                 
+//        if (array[i] > limit) n total n ^ 2 = 0(n)
 //        {
-//            Console.WriteLine(array[i]); 
+//            Console.WriteLine(array[i]); 2n
 //        }
 
 //    }
@@ -33,9 +33,9 @@
 //int FindCount(int[] arr, int number)
 //{
 //    int count = 0;
-//    for (int i = 0; i < arr.Length; i++)        //// n
-//    {                                                          ////total n^2+n^2= o(n^2)
-//        if (arr[i] == number)                   //// n^2
+//    for (int i = 0; i < arr.Length; i++)        // n
+//    {                                                        //  total 2n= o(n)
+//        if (arr[i] == number)                  //  n
 //        {
 //            count++;
 //        }
@@ -48,11 +48,11 @@
 //Console.WriteLine(result);
 
 
-//TASK - c
+//TASK - C
 
 //int[] ChangeNumbbers(int[] number)
 //{
-//    for (int i = 0; i < number.Length - 1; i += 2)
+//    for (int i = 0; i < number.Length - 1; i += 2)     ////n
 //    {
 //        int temp = number[i];
 //        number[i] = number[i + 1];
@@ -63,12 +63,12 @@
 
 //}
 
-//int[] result = ChangeNumbbers(new int[] { 1, 2, 3, 4, 5 });
+//int[] result = ChangeNumbbers(new int[] { 1, 2, 3, 4, 5 });        // n
 
 
-//for (int i = 0; i < result.Length; i++)
+//for (int i = 0; i < result.Length; i++)                            //n       total = o(n^2)
 //{
-//    Console.WriteLine(result[i]);
+//    Console.WriteLine(result[i]);                                // n
 //}
 
 
